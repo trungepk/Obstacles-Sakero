71897b9694806f7186a837946d4ef33776eb9cdd
trungepk <nevergiveup14896@gmail.com>
2018-11-10 23:54:59 +0700

    Player movement and camera following

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     [SerializeField] float speed = 2000f;
+    [SerializeField] float sidewaySpeed = 1000f;
     private Rigidbody rb;
 
 	void Start () {
         rb = GetComponent<Rigidbody>();
 	}
 	
-	void Update () {
+	void FixedUpdate () {
         rb.AddForce(0, 0, speed * Time.deltaTime);
-        		
+        var sideMovement = Input.GetAxis("Horizontal") * sidewaySpeed;
+        rb.AddForce(sideMovement * Time.deltaTime, 0, 0);
 	}
 }
