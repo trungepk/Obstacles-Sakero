5cf98f21527ecb34c8b3869178887cd1d1e07839
trungepk <nevergiveup14896@gmail.com>
2018-11-10 23:33:35 +0700

    Set up ground and player

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField] float speed = 2000f;
+    private Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        rb.AddForce(0, 0, speed * Time.deltaTime);
+        		
+	}
+}
